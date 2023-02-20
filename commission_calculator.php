dbbc992f0d7f5969e14cd0cd001edeeb8e76e89c
Rakib Hasan <mhrakibhasan313@gmail.com>
2023-02-21 00:35:58 +0600

    Assignment 2

--- commission_calculator.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$tuitionFee = 18000;
-
-$commission = ($tuitionFee > 20000) ? $tuitionFee*0.25 : ($tuitionFee > 10000 && $tuitionFee < 20000 ? $tuitionFee*0.2 : ($tuitionFee > 7000 && $tuitionFee < 10000 ? $tuitionFee*0.15 : "No Comission"));
-
-echo $commission;
